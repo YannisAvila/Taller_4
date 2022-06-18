06517bb91c266577b775ca159b62b83f4165d743
Katanac <tatisred@hotmail.com>
2022-06-17 23:59:35 -0500

    ejercicio 6

--- src/main/kotlin/Ejercicio_6.kt
+++ src/main/kotlin/Ejercicio_6.kt
@@ -1,3 +1,5 @@
+import java.util.Collections
+
 /*
 6. Desarrolle la siguiente función y programa
 a. Escriba un función que reciba una lista de pares (el primer
@@ -10,6 +12,43 @@ lista, y luego de terminar la lectura, y usando la función
 anterior, imprima el nombre del estudiante más viejo
  */
 
-fun main(){
+fun main() {
 
+    print("Ingresar cuantos estudiantes desea?  ")
+    val num_est = readln().toInt()
+    val lista_est: MutableList<Pair<String, Int>> = mutableListOf()
+
+
+    var fechNac: Int
+    var nombre: String
+
+    repeat(num_est) {
+        println("Como es el nombre del estudiante :  ")
+        nombre = readln()
+        println("Cual es el año de nacimiento :  ")
+        fechNac = readLine()!!.toInt()
+
+        lista_est.add(Pair(nombre, fechNac))
+    }
+
+    println("El estudiante mas viejo de la clase es: ${est_viejo(lista_est)}")
+}
+
+fun est_viejo(lista_est: MutableList<Pair<String, Int>>): String {
+
+    val listaEdades: MutableList<Int> = mutableListOf()
+
+    for (estudiante in lista_est) {
+        val edad = 2022 - estudiante.second
+        listaEdades.add(edad)
+    }
+
+    val mayorEdad = Collections.max(listaEdades)
+
+    for (estudiante in lista_est) {
+        val edad = 2022 - estudiante.second
+        if (edad == mayorEdad) return estudiante.first
+    }
+
+    return ""
 }
--- src/main/kotlin/Ejericio_1.kt
+++ src/main/kotlin/Ejericio_1.kt
@@ -1,28 +1,28 @@
-/*
-fun siglo(list: MutableList<Int>):Pair<Int,Double>{
-    var siglo_pasado=0
-    var suma=0.0
-    for (edad in list){
-        if (edad>=22){
+fun siglo(list: MutableList<Int>): Pair<Int, Double> {
+    var siglo_pasado = 0
+    var suma = 0.0
+    for (edad in list) {
+        if (edad >= 22) {
             siglo_pasado++
         }
-        suma+=edad
+        suma += edad
     }
-    var promedio=suma/list.size
+    val promedio = suma / list.size
     return siglo_pasado to promedio
 }
 
-fun main(){
-    var lista.MutableList<Int> = mutableListOf()
+fun main() {
+    val lista: MutableList<Int> = mutableListOf()
     print("Ingrese la edad: ")
-    var edad= readLine()!!.toInt()
-    while (edad>0){
+    val edad = readLine()!!.toInt()
+    while (edad > 0) {
         print("Ingrese la edad")
-        var edad= readLine()!!.toInt()
-        lista.add(edad)
+        val edadI = readLine()!!.toInt()
+        lista.add(edadI)
         print(lista)
     }
-    var (siglo_pasado,promedio)= siglo(lista)
+
+    val (siglo_pasado, promedio) = siglo(lista)
     println("Hay $siglo_pasado estudiante del siglo pasado")
     println("El promedio de edad de la Universidad es $promedio ")
-}*/
+}
